a4b886f370cf07ddfe7f6515543499d78da3d4a4
Rick Selby <rick@selby-family.co.uk>
2018-01-17 22:13:46 +0000

    Simplify some simple inputs

--- src/resources/views/date.blade.php
+++ src/resources/views/date.blade.php
@@ -1,9 +1,2 @@
 
-@component('fc::layout.field', ['label' => $label, 'name' => $name, 'help' => $help ?? null])
-    {{
-        html()
-            ->input('date', $name, $value ?? null)
-            ->placeholder($placeholder ?? null)
-            ->class(['form-control', 'is-invalid' => $errors->has($name)])
-    }}
-@endcomponent
+@include('fc::layout.simple', ['type' => 'date'])
---
+++ src/resources/views/layout/simple.blade.php
@@ -0,0 +1,9 @@
+
+@component('fc::layout.field', ['label' => $label, 'name' => $name, 'help' => $help ?? null])
+    {{
+        html()
+            ->input($type, $name, $value ?? null)
+            ->placeholder($placeholder ?? null)
+            ->class(['form-control', 'is-invalid' => $errors->has($name)])
+    }}
+@endcomponent
