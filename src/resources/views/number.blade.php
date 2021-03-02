c98bac0e6603d9b919ba589895709eeeeed5abf5
Rick Selby <richard.selby@york.ac.uk>
2021-03-02 09:23:32 +0000

    Min and Max could be zero...

--- src/resources/views/number.blade.php
+++ src/resources/views/number.blade.php
@@ -8,8 +8,8 @@
             ->class($class ?? [])
             ->invalidClass($errors, $name)
             ->attributeIf($step ?? null, 'step', $step ?? null)
-            ->attributeIf($min ?? null, 'min', $min ?? null)
-            ->attributeIf($max ?? null, 'max', $max ?? null)
+            ->attributeIf(($min ?? null) !== null, 'min', $min ?? null)
+            ->attributeIf(($max ?? null) !== null, 'max', $max ?? null)
             ->addData($data ?? null)
     }}
 @endcomponent
